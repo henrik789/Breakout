9e39f0488d57fddd56fe26f668c0c3ec6c5bf1f6
Henrik789 <henrik789@github.com>
2018-12-10 21:08:29 +0100

    Trying a shape node

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -11,67 +11,123 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+//    var entities = [GKEntity]()
+//    var graphs = [String : GKGraph]()
+//
+//    private var lastUpdateTime : TimeInterval = 0
+//    private var label : SKLabelNode?
+//    private var spinnyNode : SKShapeNode?
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    override func didMove(to view: SKView) {
+        let color = UIColor.gray
+        backgroundColor = color
+            setupPlayerAndObstacles()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        for i in 1...5{
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        let number = Int.random(in: 1...20)
+        print(number)
         }
     }
+    
+    func setupPlayerAndObstacles() {
+        addObstacle()
+    }
+    
+    func addObstacle() {
+        addCircleObstacle()
+    }
+    
+    func addCircleObstacle() {
+        // 1
+        let path = UIBezierPath()
+        // 2
+//        path.move(to: CGPoint(x: 0, y: 200))
+        // 3
+//        path.addLine(to: CGPoint(x: 0, y: 0))
+        // 4
+        path.addArc(withCenter: CGPoint.zero,
+                    radius: 60,
+                    startAngle: CGFloat(1.0 * .pi / 2),
+                    endAngle: CGFloat(0),
+                    clockwise: true)
+        // 5
+        path.addLine(to: CGPoint(x: 200, y: 0))
+        path.addArc(withCenter: CGPoint.zero,
+                    radius: 200,
+                    startAngle: CGFloat(0),
+                    endAngle: CGFloat(1.0 * .pi / 2),
+                    clockwise: false)
+        
+        let section = SKShapeNode(path: path.cgPath)
+        section.position = CGPoint(x: size.width/2, y: size.height/2)
+        section.fillColor = .white
+        section.strokeColor = .black
+        addChild(section)
+        
+        let duration = 1.0
+        let open = SKAction.fadeOut(withDuration: duration)
+//        let wait = SKAction.wait(forDuration: duration)
+        let close = SKAction.fadeIn(withDuration: duration * 2)
+        let sequence = SKAction.sequence([open, close, open, close])
+        
+    section.run(SKAction.repeatForever(sequence))
+    }
+    
+//    override func sceneDidLoad() {
+//
+//        self.lastUpdateTime = 0
+//
+//        // Get label node from scene and store it for use later
+//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+//        if let label = self.label {
+//            label.alpha = 0.0
+//            label.run(SKAction.fadeIn(withDuration: 2.0))
+//        }
+//
+//        // Create shape node to use during mouse interaction
+//        let w = (self.size.width + self.size.height) * 0.05
+//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//
+//        if let spinnyNode = self.spinnyNode {
+//            spinnyNode.lineWidth = 2.5
+//
+//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+//                                              SKAction.fadeOut(withDuration: 0.5),
+//                                              SKAction.removeFromParent()]))
+//        }
+//    }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//            self.addChild(n)
+//        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.blue
+//            self.addChild(n)
+//        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.red
+//            self.addChild(n)
+//        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
@@ -93,18 +149,20 @@ class GameScene: SKScene {
         // Called before each frame is rendered
         
         // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
-    }
+//        if (self.lastUpdateTime == 0) {
+//            self.lastUpdateTime = currentTime
+//        }
+//
+//        // Calculate time since last update
+//        let dt = currentTime - self.lastUpdateTime
+//
+//        // Update entities
+//        for entity in self.entities {
+//            entity.update(deltaTime: dt)
+//        }
+//
+//        self.lastUpdateTime = currentTime
+//    }
 }
+}
+
